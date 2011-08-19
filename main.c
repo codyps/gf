5c5c693d34878448a4529d642b169a97955f9306
Cody Schafer <cpschafer@gmail.com>
2011-08-18 20:18:44 -0400

    try and fail to draw grid.

--- main.c
+++ main.c
@@ -1,12 +1,59 @@
 #include <stdio.h>
+#include <stdint.h>
 
 #include <SDL/SDL.h>
+#include <SDL/SDL_ttf.h>
+
 #include <GL/gl.h>
 
 #define INIT_W 840
 #define INIT_V 640
 
+#define RECT_EDGE 10
+
+#define GRID_W 100
+#define GRID_H 100
+
+
+struct tile {
+	SDL_Rect rect;
+
+	uint32_t color;
+};
+
+static struct tile grid[GRID_H * GRID_W];
+
 static SDL_Surface *screen;
+
+static void grid_init(struct tile *grid, int width, int vert)
+{
+	int v, v_off;
+	for (v = 0, v_off = 0; v < vert; v++, v_off += width) {
+		int w;
+		for (w = 0; w < width; w++) {
+			struct tile *tile = grid + v * width + w;
+			SDL_Rect *r = &tile->rect;
+			r->w = RECT_EDGE;
+			r->h = RECT_EDGE;
+			r->x = w * RECT_EDGE;
+			r->y = v * RECT_EDGE;
+
+			tile->color = w << 16 | v;
+		}
+	}
+}
+
+static void grid_draw(struct tile *grid, int width, int vert)
+{
+	int v;
+	for (v = 0; v < vert; v++) {
+		int w;
+		for (w = 0; w < width; w++) {
+			struct tile *tile = grid + v * width + w;
+			SDL_FillRect(screen, &tile->rect, tile->color);
+		}
+	}
+}
 
 static int init(void)
 {
@@ -34,19 +81,21 @@ static int init(void)
 
 static void fini(void)
 {
-
 	SDL_FreeSurface(screen);
 
 	SDL_Quit();
 }
 
 #define __unused __attribute__((__unused__))
-
 int main(__unused int argc, __unused char **argv)
 {
 	SDL_Event event;
 
 	init();
+
+	grid_init(grid, GRID_W, GRID_H);
+	grid_draw(grid, GRID_W, GRID_H);
+
 	SDL_WM_SetCaption( "Event test", NULL );
 
 	for(;;) {
